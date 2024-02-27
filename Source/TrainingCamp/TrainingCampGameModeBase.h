483506c96a99ab2e5b6741c0bc57f6b8760e9e29
LocalMachine <157286208+MaquinaLocal@users.noreply.github.com>
2024-02-27 20:44:04 -0300

    First Person Project
    First Commit

---
+++ Source/TrainingCamp/TrainingCampGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrainingCampGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAININGCAMP_API ATrainingCampGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
