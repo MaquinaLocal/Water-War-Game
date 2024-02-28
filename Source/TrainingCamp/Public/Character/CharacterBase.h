0b7d0cb76ff7bc94ea64ce8a5b197e2f29fa3626
LocalMachine <157286208+MaquinaLocal@users.noreply.github.com>
2024-02-28 12:45:31 -0300

    Finalización Aspectos Basicos de FPcharacter

--- Source/TrainingCamp/Public/Character/CharacterBase.h
+++ Source/TrainingCamp/Public/Character/CharacterBase.h
@@ -4,26 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
 #include "CharacterBase.generated.h"
 
-class UInputComponent;
-class USkeletalMeshComponent;
-class UCameraComponent;
 
 UCLASS()
 class TRAININGCAMP_API ACharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Mesh para el Pawn principal (brazos) **/
+	/** Mesh para el Pawn principal (brazos) */
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
 	USkeletalMeshComponent* FirstPersonMesh;
 
-	/** Mesh para el arma principal **/
+	/** Mesh para el arma principal */
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
 	USkeletalMeshComponent* WeaponMesh;
 
-	/** Cámara primera persona **/
+	/** Cámara primera persona */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FirstPersonCameraComponent;
 
@@ -31,35 +29,23 @@ public:
 	// Sets default values for this character's properties
 	ACharacterBase();
 
-	/** Cámara: Tasa de giro horizontal (en grados por segundo) **/
+	/** Cámara: Tasa de giro horizontal (en grados por segundo) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseTurnRate;
 
-	/** Cámara: Tasa de giro vertical **/
+	/** Cámara: Tasa de giro vertical */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseLookUpRate;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	/** Movimiento lateral **/
+	
+	/** Movimiento lateral */
 	void MoveRight(float Val);
 
-	/** Movimiento frontal **/
+	/** Movimiento frontal */
 	void MoveForward(float Val);
 
-	/** Camara: tasa de giro lateral **/
-	void TurnAtRate(float Val);
-
-	/** Camara: tasa de giro vertical **/
-	void LookUpRate(float Val);
-
-	/** Interfaz APawn para movimiento **/
+	/** Interfaz APawn para movimiento */
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
