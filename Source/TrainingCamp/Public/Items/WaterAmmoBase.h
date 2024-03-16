91d90ea7a4bb9d532929ea06dbb416c43e95c029
LocalMachine <157286208+MaquinaLocal@users.noreply.github.com>
2024-03-16 09:31:40 -0300

    Now you can recharge the water level

---
+++ Source/TrainingCamp/Public/Items/WaterAmmoBase.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AffectPlayer.h"
+#include "FirstPersonPC.h"
+#include "WaterAmmoBase.generated.h"
+
+UCLASS()
+class TRAININGCAMP_API AWaterAmmoBase : public AActor, public IAffectPlayer
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWaterAmmoBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void GetPlayerWaterRefill(AActor* Player) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
